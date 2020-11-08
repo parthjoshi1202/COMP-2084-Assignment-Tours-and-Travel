5e6e39063c358534dea889ce3761c3bed09c58ea
Parth Joshi <pjoshi4@lakeheadu.ca>
2020-11-07 23:49:53 -0500

    Modifying the site so that CRUD Functionality is accessed by authenticated users

--- ToursandTravel/Controllers/PackageTypesController.cs
+++ ToursandTravel/Controllers/PackageTypesController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -10,6 +11,7 @@ using ToursandTravel.Models;
 
 namespace ToursandTravel.Controllers
 {
+    [Authorize]
     public class PackageTypesController : Controller
     {
         private readonly ApplicationDbContext _context;
@@ -26,6 +28,7 @@ namespace ToursandTravel.Controllers
         }
 
         // GET: PackageTypes/Details/5
+        [AllowAnonymous]
         public async Task<IActionResult> Details(int? id)
         {
             if (id == null)
--- ToursandTravel/Controllers/PackagesController.cs
+++ ToursandTravel/Controllers/PackagesController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
@@ -12,6 +13,7 @@ using ToursandTravel.Models;
 
 namespace ToursandTravel.Controllers
 {
+    [Authorize]
     public class PackagesController : Controller
     {
         private readonly ApplicationDbContext _context;
